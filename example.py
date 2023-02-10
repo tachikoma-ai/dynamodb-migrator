4f6df24fe633538130e04ec18a25182c6c6cece3
Adrien Carpentier <me@adriencarpentier.com>
2023-02-10 17:51:31 +0900

    Update example.py

--- example.py
+++ example.py
@@ -6,8 +6,8 @@ EXPORT_PATH: str = "exports/"  # with no slash at the beginning
 
 # Export data from the export table
 export_file_path: str = export_ddb(
-    path=EXPORT_PATH, timestamp=True, db_name_export=None
+    path=EXPORT_PATH, timestamp=True, db_name_export="my-ddb-table-name-to-export-from"
 )
 
 # Import data into the import table
-import_ddb_from_ddb_json(file_path=export_file_path, db_name_import=None, fake=True)
+import_ddb_from_ddb_json(file_path=export_file_path, db_name_import="my-ddb-table-name-to-import-to", fake=True)
